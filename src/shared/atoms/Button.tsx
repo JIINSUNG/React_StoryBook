b289745458a6c832d139bc7f6ee49b85d824a5d4
JIINSUNG <crm03008@gmail.com>
2024-08-30 01:17:56 +0900

    refactor: 버튼 컴포넌트 코드 간결화

--- src/shared/atoms/Button.tsx
+++ src/shared/atoms/Button.tsx
@@ -1,31 +1,42 @@
 import React from "react";
 
 export interface ButtonProps {
-  label: string;
-  backgroundColor?: string;
-  size?: "sm" | "md" | "lg";
-  onClick?: () => void;
-  className?: string;
+    label: React.ReactNode;
+    backgroundColor?: "red" | "blue" | "green" | "yellow";
+    size?: "sm" | "md" | "lg";
+    onClick?: () => void;
+    className?: string;
 }
 
-export const Button: React.FC<ButtonProps> = ({
-  label,
-  backgroundColor = "bg-blue-500",
-  size = "md",
-  onClick,
-  className,
-}) => {
-  const baseClasses =
-    "text-white font-bold rounded focus:outline-none focus:ring-2 focus:ring-blue-400";
-  const sizeClasses =
-    size === "lg" ? "py-3 px-6" : size === "sm" ? "py-1 px-2" : "py-2 px-4";
+const sizeClasses = {
+    lg: "py-3 px-6",
+    md: "py-2 px-4",
+    sm: "py-1 px-2",
+} as const;
 
-  return (
-    <button
-      className={`${baseClasses} ${sizeClasses} ${backgroundColor} ${className}`}
-      onClick={onClick}
-    >
-      {label}
-    </button>
-  );
+const backgroundClasses = {
+    blue: "bg-blue-500",
+    red: "bg-red-500",
+    green: "bg-green-500",
+    yellow: "bg-yellow-500",
+} as const;
+
+const baseClasses =
+    "text-white font-bold rounded focus:outline-none focus:ring-2";
+
+export const Button: React.FC<ButtonProps> = ({
+    label,
+    backgroundColor = "blue",
+    size = "md",
+    onClick,
+    className,
+}) => {
+    return (
+        <button
+            className={`${baseClasses} ${sizeClasses[size]} ${backgroundClasses[backgroundColor]} ${className}`}
+            onClick={onClick}
+        >
+            {label}
+        </button>
+    );
 };
