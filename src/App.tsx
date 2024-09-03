047dc84ca0a21485dbf4b01cc634f2ecc2eed01f
JIINSUNG <crm03008@gmail.com>
2024-09-03 17:07:53 +0900

    feat: 이미지 컴포넌트 추가 및 테스트 코드 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import "./App.css";
+import { BrandCard } from "./shared/atoms/BrandCard.tsx";
 import { Button } from "./shared/atoms/Button.tsx";
+import Image from "./shared/atoms/Image.tsx";
 import "./tailwindcss.css";
 function App() {
-    return <Button label="button" backgroundColor="blue" />;
+  return (
+    <div>
+      <Button label="button" backgroundColor="blue" />
+      <Image src="https://picsum.photos/200/300" label="Image" />
+      <BrandCard src="https://picsum.photos/200/300" alt="Image" />
+    </div>
+  );
 }
 
 export default App;
