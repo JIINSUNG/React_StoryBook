efd18f491266b2a7a99b3033b6c86e6533501966
JIINSUNG <crm03008@gmail.com>
2024-09-04 10:16:48 +0900

    feat: Typography 컴포넌트

---
+++ src/shared/atoms/components/Typography.stories.tsx
@@ -0,0 +1,46 @@
+import { TypographyProps, Typography } from "./Typography.tsx";
+import type { StoryObj, Meta, StoryFn } from "@storybook/react";
+
+export default {
+  title: "Test/Typography",
+  component: Typography,
+  argTypes: {
+    size: {
+      control: {
+        type: "select",
+        options: ["head", "subhead", "paragraph"],
+      },
+    },
+    children: {
+      control: {
+        type: "text",
+      },
+    },
+  },
+  args: {
+    size: "head",
+    children: "Text 컴포넌트",
+  },
+} as Meta<typeof Typography>;
+
+const Template: StoryFn<TypographyProps> = (args: TypographyProps) => (
+  <Typography {...args} />
+);
+
+export const Head: StoryObj<typeof Typography> = Template.bind({});
+Head.args = {
+  size: "head",
+  children: "Head",
+};
+
+export const SubHead: StoryObj<typeof Typography> = Template.bind({});
+SubHead.args = {
+  size: "subhead",
+  children: "SubHead",
+};
+
+export const Paragraph: StoryObj<typeof Typography> = Template.bind({});
+Paragraph.args = {
+  size: "paragraph",
+  children: "Paragraph",
+};
---
+++ src/shared/atoms/components/Typography.tsx
@@ -0,0 +1,27 @@
+import React, { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
+
+export type TypographyProps = {
+  className?: string;
+  size?: "head" | "subhead" | "paragraph" | "title";
+  children: ReactNode;
+};
+
+const TypographyStyle = {
+  head: "text-[2rem] font-bold",
+  subhead: "text-[1.5rem] font-bold",
+  title: "text-[1rem] font-bold",
+  paragraph: "text-[1rem]",
+} as const;
+
+export const Typography: React.FC<TypographyProps> = ({
+  className,
+  size = "paragraph",
+  children,
+}: TypographyProps) => {
+  return (
+    <text className={twMerge(className, TypographyStyle[size])}>
+      {children}
+    </text>
+  );
+};
