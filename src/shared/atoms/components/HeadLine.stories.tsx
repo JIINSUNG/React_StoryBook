de2e78c625fccdc906982b108403796b0201b2ed
JIINSUNG <crm03008@gmail.com>
2024-09-11 23:38:50 +0900

    add: 상품 헤드라인 컴포넌트

--- src/shared/atoms/components/HeadLine.stories.tsx
+++ src/shared/atoms/components/HeadLine.stories.tsx
@@ -4,13 +4,25 @@ import type { StoryObj, Meta, StoryFn } from "@storybook/react";
 export default {
   title: "Test/HeadLine",
   component: HeadLine,
-  argTypes: {},
-  args: {},
+  argTypes: {
+    pName: {
+      control: {
+        type: "text",
+      },
+    },
+  },
 } as Meta<typeof HeadLine>;
 
 const Template: StoryFn<HeadLineProps> = (args: HeadLineProps) => (
   <HeadLine {...args} />
 );
 
-export const PropertyName: StoryObj<typeof HeadLine> = Template.bind({});
-PropertyName.args = {};
+export const BasicHeadLine: StoryObj<typeof HeadLine> = Template.bind({});
+BasicHeadLine.args = {
+  pName: "",
+};
+
+export const BasicHeadLineWithName: StoryObj<typeof HeadLine> = Template.bind(
+  {},
+);
+BasicHeadLineWithName.args = { pName: "상품명" };
--- src/shared/atoms/components/HeadLine.tsx
+++ src/shared/atoms/components/HeadLine.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { twMerge } from "tailwind-merge";
+import { Typography } from "./Typography.tsx";
+import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 
 export type HeadLineProps = {
   className?: string;
+  pName?: string;
+  onClick?: () => void;
 };
 
-export const HeadLine: React.FC<HeadLineProps> = ({ className }) => {
-  return <div className={className}></div>;
+export const HeadLine: React.FC<HeadLineProps> = ({
+  className,
+  pName,
+  onClick,
+}: HeadLineProps) => {
+  return (
+    <header className={twMerge(className, "flex justify-between p-5")}>
+      <KeyboardBackspaceIcon onClick={onClick} />
+      {pName && <Typography size="subhead">{pName}</Typography>}
+      <div className="invisible"></div>
+    </header>
+  );
 };
